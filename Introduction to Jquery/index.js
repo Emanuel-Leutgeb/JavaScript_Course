96b28c88c3d58eb561f220a31e31a64055f0a935
Emanuel Leutgeb <emanuel@leutgeb.com>
2022-01-20 10:30:25 +0100

    Introduction to Jquery

---
+++ Introduction to Jquery/index.js
@@ -0,0 +1,14 @@
+//$('h1').hide();
+//$('h1').show();
+//$("h1#firstHeading").hide();
+//$('.secondHeading').hide();
+$("p").css("fontSize", "20px");
+$("body").css("background-color", "black").css("color", "red");
+$("ul#myList li:first").css("color", "yellow");
+$("ul#myList li:last").css("color", "blue").css("fontSize", "40px");
+$("ul#myList li:nth-child(3)").css("color", "pink");
+$("ul#myList li:nth-child(2n)").css("color", "grey");
+$("ul#myList li:even").css("backgroundColor", "green"); //odd - even
+//$("*").hide();
+
+
